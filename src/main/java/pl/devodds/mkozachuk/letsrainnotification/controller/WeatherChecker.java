63f2b07b554c7b935483cbc2fb7e1e98eb9f786e
Maksym <maksym.kozachuk@protonmail.com>
2020-09-02 15:08:46 +0200

    add processing for current weather + forecast weather add daily rain analyzer

--- src/main/java/pl/devodds/mkozachuk/letsrainnotification/controller/WeatherChecker.java
+++ src/main/java/pl/devodds/mkozachuk/letsrainnotification/controller/WeatherChecker.java
@@ -3,9 +3,19 @@ package pl.devodds.mkozachuk.letsrainnotification.controller;
 import lombok.extern.slf4j.Slf4j;
 import net.aksingh.owmjapis.api.APIException;
 import net.aksingh.owmjapis.core.*;
+import net.aksingh.owmjapis.model.CurrentWeather;
+import net.aksingh.owmjapis.model.HourlyWeatherForecast;
+import net.aksingh.owmjapis.model.param.WeatherData;
+import org.json.JSONArray;
 import org.json.JSONObject;
 import org.springframework.stereotype.Controller;
-import pl.devodds.mkozachuk.letsrainnotification.model.CurrentWeather;
+import pl.devodds.mkozachuk.letsrainnotification.model.Weather;
+
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.time.LocalDate;
+import java.time.ZoneId;
+import java.util.*;
 
 @Slf4j
 @Controller
@@ -17,7 +27,7 @@ public class WeatherChecker {
         JSONObject allCurrentlyWeatherData;
         try {
             allCurrentlyWeatherData = new JSONObject(owm.currentWeatherByCityId(cityId));
-        }catch (APIException e){
+        } catch (APIException e) {
             log.error("Wheather API Exception : {}", e.getInfo());
             e.printStackTrace();
             allCurrentlyWeatherData = new JSONObject();
@@ -25,12 +35,12 @@ public class WeatherChecker {
         return allCurrentlyWeatherData;
     }
 
-    public JSONObject getHourlyWeatherData(int cityId){
+    public JSONObject getHourlyWeatherData(int cityId) {
         owm.setUnit(OWM.Unit.METRIC);
         JSONObject allHourlyWeatherData;
         try {
-        allHourlyWeatherData = new JSONObject(owm.hourlyWeatherForecastByCityId(cityId));
-        }catch (APIException e){
+            allHourlyWeatherData = new JSONObject(owm.hourlyWeatherForecastByCityId(cityId));
+        } catch (APIException e) {
             log.error("Wheather API Exception : {}", e.getInfo());
             e.printStackTrace();
             allHourlyWeatherData = new JSONObject();
@@ -38,32 +48,81 @@ public class WeatherChecker {
         return allHourlyWeatherData;
     }
 
-    public CurrentWeather currentWeatherMaker(JSONObject currentWeatherData){
-        CurrentWeather currentWeather = new CurrentWeather();
-        JSONObject maindata = new JSONObject(currentWeatherData.getString("mainData"));
-        currentWeather.setCurrentTemp(maindata.getDouble("temp"));
-        currentWeather.setHumidity(maindata.getInt("humidity"));
-        currentWeather.setPressure(maindata.getInt("pressure"));
-        maindata = new JSONObject(currentWeatherData.getString("weatherList"));
-        currentWeather.setIconLink(maindata.getString("iconLink"));
-        currentWeather.setConditionId(maindata.getInt("conditionId"));
-        currentWeather.setDescription(maindata.getString("description"));
-        maindata = new JSONObject(currentWeatherData.getString("cloudData"));
-        currentWeather.setCloudPercent(maindata.getInt("cloud"));
-        maindata = null;
-        return currentWeather;
+    public Weather currentWeatherMaker(CurrentWeather currentWeatherData) {
+        Weather weather = new Weather();
+
+        if(currentWeatherData.hasMainData()) {
+            weather.setCurrentTemp(currentWeatherData.getMainData().getTemp());
+            weather.setHumidity(currentWeatherData.getMainData().getHumidity().intValue());
+            weather.setPressure(currentWeatherData.getMainData().getPressure().intValue());
+        }else {
+            log.error("NPE There is no MainData");
+        }
+        if(currentWeatherData.hasWeatherList()) {
+            weather.setIconLink(currentWeatherData.getWeatherList().get(0).getIconLink());
+            weather.setConditionId(currentWeatherData.getWeatherList().get(0).getConditionId());
+            weather.setDescription(currentWeatherData.getWeatherList().get(0).getDescription());
+        }else {
+            log.error("NPE There is no WeatherList");
+        }
+        weather.setCloudPercent(currentWeatherData.getCloudData().getCloud().intValue());
+        return weather;
     }
 
-
-
-
-    public boolean isItRainyToday (){
-        if(true){
-            return true;
+    public List<Weather> hourlyWeatherCreator(HourlyWeatherForecast hourlyForecast) {
+        List<Weather> hourlyWeather = new ArrayList<>();
+        List<WeatherData> dates = new ArrayList<>(hourlyForecast.getDataList());
+        for (int i = 0; i < dates.size(); i++) {
+            Weather weather = new Weather();
+            weather.setDate(dates.get(i).getDateTime());
+            if(dates.get(i).hasMainData()) {
+            weather.setCurrentTemp(dates.get(i).getMainData().getTemp());
+            weather.setHumidity(dates.get(i).getMainData().getHumidity().intValue());
+            weather.setPressure(dates.get(i).getMainData().getPressure().intValue());
+            }else {
+                log.error("NPE There is no MainData");
+            }
+            if(dates.get(i).hasWeatherList()) {
+            weather.setIconLink(dates.get(i).getWeatherList().get(0).getIconLink());
+            weather.setConditionId(dates.get(i).getWeatherList().get(0).getConditionId());
+            weather.setDescription(dates.get(i).getWeatherList().get(0).getDescription());
+            }else {
+                log.error("NPE There is no WeatherList");
+            }
+            weather.setCloudPercent(dates.get(i).getCloudData().getCloud().intValue());
+            hourlyWeather.add(weather);
         }
-        else{
-            return false;
+
+        return hourlyWeather;
+
+    }
+
+    public boolean isItRainyToday(List<Weather> hourlyWeather) {
+
+        boolean rainy = false;
+        Date currentDate = new Date();
+        LocalDate localDate = currentDate.toInstant().atZone(ZoneId.systemDefault()).toLocalDate();
+        int today = localDate.getDayOfMonth();
+        LocalDate forecastDate;
+        int forecastDay;
+
+        List<Weather> todayHourly = new ArrayList<>();
+        for(Weather hourly : hourlyWeather){
+            forecastDate = hourly.getDate().toInstant().atZone(ZoneId.systemDefault()).toLocalDate();
+            forecastDay = forecastDate.getDayOfMonth();
+            if(forecastDay == today){
+                todayHourly.add(hourly);
+            }
         }
+        System.out.println(todayHourly);
+
+        for(Weather todayWeather : todayHourly){
+            if(todayWeather.getDescription().contains("rain")){
+                rainy = true;
+            }
+        }
+
+        return rainy;
     }
 
 
